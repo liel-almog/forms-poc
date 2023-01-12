59b51c8874b99a76e72726389ae69c771e7031bc
Liel Almog <liel.almogg@gmail.com>
2023-01-12 22:02:27 +0200

    Add two examples for components with react-hook-form state changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { useForm, useFormState } from "react-hook-form";
+import { Input, InputTwo } from "./components/Input";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { handleSubmit, register, control, setValue } = useForm({
+    defaultValues: {
+      test: "",
+      test2: "",
+      firstName: "asd",
+    },
+  });
 
+  const { dirtyFields } = useFormState({
+    control,
+  });
+
+  const onSubmit = (data: any) => alert(JSON.stringify(data));
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("firstName")} placeholder="First Name" />
+        <Input register={register} name="test" />
+        <InputTwo setValue={setValue} register={register} name="test2" />
+        {dirtyFields.firstName && dirtyFields.test && <p>Field is dirty.</p>}
+        <input type="submit" />
+      </form>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,33 @@
-import classes from './input.module.scss';
+import { ChangeEvent } from "react";
+import { useForm, UseFormRegister, UseFormSetValue, useFormState } from "react-hook-form";
 
-export interface InputProps {}
+export interface InputProps {
+  name: string;
+  register: UseFormRegister<any>;
+}
 
 export const Input = (props: InputProps) => {
-	
-	return (
-		<div></div>
-	);
+  return (
+    <input
+      {...props.register(props.name, {
+        onChange(event) {
+          console.log("onChange", event.target.value);
+        },
+      })}
+      placeholder="test"
+    />
+  );
+};
+
+interface InputTwoProps extends InputProps {
+  setValue: UseFormSetValue<any>;
+}
+
+export const InputTwo = (props: InputTwoProps) => {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    props.setValue(props.name, e.target.value);
+  };
+  const { onChange: _onChange, onBlur: _onBlue, ...methods } = props.register(props.name);
+  return <input {...methods} onChange={onChange} placeholder="test2" />;
 };
